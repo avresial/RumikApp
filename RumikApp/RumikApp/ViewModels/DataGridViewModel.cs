858dce70a6f9403e137c8eecd3920db07e5436e7
Mikołaj Karkowski <mikikarkowski@gmail.com>
2021-03-18 00:03:17 +0100

    Update - added unknown milord for no data in DataGridViewModel

--- RumikApp/RumikApp/ViewModels/DataGridViewModel.cs
+++ RumikApp/RumikApp/ViewModels/DataGridViewModel.cs
@@ -25,6 +25,34 @@ namespace RumikApp.ViewModels
             }
         }
 
+        private Visibility _ScrollViewerVisibility;
+        public Visibility ScrollViewerVisibility
+        {
+            get { return _ScrollViewerVisibility; }
+            set
+            {
+                if (_ScrollViewerVisibility == value)
+                    return;
+
+                _ScrollViewerVisibility = value;
+                RaisePropertyChanged("ScrollViewerVisibility");
+            }
+        }
+
+        private Visibility _UnknownGuyVisibility;
+        public Visibility UnknownGuyVisibility
+        {
+            get { return _UnknownGuyVisibility; }
+            set
+            {
+                if (_UnknownGuyVisibility == value)
+                    return;
+
+                _UnknownGuyVisibility = value;
+                RaisePropertyChanged("UnknownGuyVisibility");
+            }
+        }
+
         private ObservableCollection<Beverage> _Beverages = new ObservableCollection<Beverage>();
         public ObservableCollection<Beverage> Beverages
         {
@@ -33,6 +61,19 @@ namespace RumikApp.ViewModels
             {
                 if (_Beverages == value)
                     return;
+
+                if (value.Count > 0)
+                {
+                    ScrollViewerVisibility = Visibility.Visible;
+                    UnknownGuyVisibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    ScrollViewerVisibility = Visibility.Collapsed;
+                    UnknownGuyVisibility = Visibility.Visible;
+                }
+
+
 
                 _Beverages = value;
                 RaisePropertyChanged("Beverages");
