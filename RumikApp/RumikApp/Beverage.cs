c5b1903384e6c5c84d0dcf4809d74e54c8ac2010
MKarkowski <mikikarkowski@gmail.com>
2021-03-18 23:37:26 +0100

    Added loading image to byte[] feature - experimental thing

--- RumikApp/RumikApp/Beverage.cs
+++ RumikApp/RumikApp/Beverage.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace RumikApp
 {
@@ -235,6 +238,20 @@ namespace RumikApp
             }
         }
 
+        private BitmapImage _TestIcon;
+        public BitmapImage TestIcon
+        {
+            get { return _TestIcon; }
+            set
+            {
+                if (_TestIcon == value)
+                    return;
+
+                _TestIcon = value;
+                RaisePropertyChanged("TestIcon");
+            }
+        }
+
         private ObservableCollection<String> _Flavours;
         public ObservableCollection<String> Flavours
         {
@@ -249,6 +266,10 @@ namespace RumikApp
             }
         }
 
+        public Beverage()
+        {
+            loadImage();
+        }
 
         public Beverage GetRandomBevrage(Random rand)
         {
@@ -276,5 +297,63 @@ namespace RumikApp
             _Price = rand.Next(0, 200);
             return this;
         }
+
+        void loadImage()
+        {
+            string imagePath = Environment.CurrentDirectory + "\\IMG\\Unknown.png";
+           
+
+            if (File.Exists(imagePath))
+            {
+                // Create image element to set as icon on the menu element
+
+                BitmapImage bmImage = new BitmapImage();
+                //bmImage.BeginInit();
+                //bmImage.UriSource = new Uri(imagePath, UriKind.Absolute);
+                //bmImage.EndInit();
+                //TestIcon = bmImage;
+
+                byte[] array = FileToByteArray(imagePath);
+                TestIcon = ConvertToBitMapImage(array);
+
+                //BitmapImage bImg = new BitmapImage();
+                //bImg.Source = ConvertToBitMapImage(bytes);
+                //byte[] bytes = ImageToByte(bImg);
+
+
+            }
+
+        
+        }
+        public static BitmapImage ConvertToBitMapImage(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(bytes))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            //image.Freeze();
+            return image;
+        }
+        public static byte[] FileToByteArray(string fileName)
+        {
+            byte[] fileData = null;
+
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                var binaryReader = new BinaryReader(fs);
+                fileData = binaryReader.ReadBytes((int)fs.Length);
+            }
+            return fileData;
+        }
+
+
     }
 }
