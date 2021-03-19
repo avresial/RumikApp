c1464356034b2f30aa95c4c1c442f91493109f01
Mikołaj Karkowski <mikikarkowski@gmail.com>
2021-03-19 18:19:30 +0100

    Update - trying converting img to byte[]

--- RumikApp/RumikApp/Services/DatabaseConnectionService.cs
+++ RumikApp/RumikApp/Services/DatabaseConnectionService.cs
@@ -118,6 +118,36 @@ namespace RumikApp.Services
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
 
+        void SaveImageToDatabase() 
+        {
+            using (MySqlConnection con = new MySqlConnection(CnnVal("sosek")))
+            {
+                byte[] img = new byte[5];
+
+                // Notice that you are missing the third field (the image one)
+                // Please replace Image with the correct name of the image field in your table
+                string query = @"INSERT INTO example (Name, Description, Image) 
+                 VALUES (@name, @description, @img";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                //cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = u;
+                //cmd.Parameters.Add("@description", MySqlDbType.VarChar).Value = v;
+                cmd.Parameters.Add("@img", MySqlDbType.Binary).Value = img;
+                con.Open();
+                // Do not execute the query two times.
+                // cmd.ExecuteNonQuery();
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    
+                    int id = (int)cmd.LastInsertedId;
+
+                }
+                else
+                {
+                    // failure msg ?
+                }
+                con.Close();
+            }
+        }
      
     }
 }
