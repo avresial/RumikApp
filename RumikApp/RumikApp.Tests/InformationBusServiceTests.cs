8422f50c89da24ab29e2b1ece4cccde7168d12a2
MKarkowski <mikikarkowski@gmail.com>
2021-04-03 13:39:25 +0200

    Added first test for RumikApp

---
+++ RumikApp/RumikApp.Tests/InformationBusServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using RumikApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xunit;
+
+namespace RumikApp.Tests
+{
+    public class InformationBusServiceTests
+    {
+        [Fact]
+        public void Beverages_IsEmpty() 
+        {
+            // Arrange
+            Visibility expectedVisibilityIsBeverageEmpty = Visibility.Visible;
+            Visibility expectedVisibilityIsBeverageNotEmpty = Visibility.Collapsed;
+
+            // Act
+            InformationBusService informationBusService = new InformationBusService();
+            informationBusService.Beverages = new ObservableCollection<Beverage>();
+            informationBusService.Beverages.Clear();
+
+            Visibility actualVisibilityIsBeverageEmpty = informationBusService.IsBeverageEmpty;
+            Visibility actualVisibilityIsBeverageNotEmpty = informationBusService.IsBeverageNotEmpty;
+
+
+            // Assert
+            Assert.Equal(expectedVisibilityIsBeverageEmpty, actualVisibilityIsBeverageEmpty);
+            Assert.Equal(expectedVisibilityIsBeverageNotEmpty, actualVisibilityIsBeverageNotEmpty);
+        }
+    }
+}
